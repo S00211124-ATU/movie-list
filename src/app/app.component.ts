8bd386d40549457d4944823ca036aa4d2a6e9617
ohnoSnoopy <s00211124@mail.itsligo.ie>
2022-02-08 16:05:28 +0000

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Directive, VERSION } from '@angular/core';
+import { MovieService } from './movie.service';
 
 @Component({
   selector: 'my-app',
@@ -6,12 +7,13 @@ import { Component, Directive, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  title = 'movie-list';
+  movies=[];
 
-  movieList = [
-    { id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola'},
-    { id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'},
-    { id: 3, title: 'Diam M for Murder', year: '1954', director: 'Alfred Hitchcock'},
-    { id: 4, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}  
-  ];
+  constructor (private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.movies = this.movieService.getMovies();
+    console.log(this.movies);
+  }
+  
 }
