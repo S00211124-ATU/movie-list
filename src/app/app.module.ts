9904bb41009257f23f18a19d898f6d182e2f5351
ohnoSnoopy <s00211124@mail.itsligo.ie>
2022-02-15 16:31:44 +0000

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AddmovieComponent } from './add-movie.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
