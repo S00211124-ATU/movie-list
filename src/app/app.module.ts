eabcc4f246cb5fe31a24229a51b0d4fc57ddf0c6
ohnoSnoopy <s00211124@mail.itsligo.ie>
2022-02-15 16:38:12 +0000

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { AddMovieComponent } from './add-movie/add_movie.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AddMovieComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
